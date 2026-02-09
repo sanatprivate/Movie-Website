5fffd045a7f064de9dcbe7d1512c6702a7d4f86f
sanatprivate <sanatplus.go@gmail.com>
2026-02-09 07:10:03 +0500

    feat: connect frontend to live backend API

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://movie-websitee-o4mb.onrender.com/api';
 
 // Utility to get user info from local storage
 const getUser = () => {
